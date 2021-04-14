906c47115a972861ba00c1fa336184535f2d269e
Kevin <kevinburchfi@gmail.com>
2021-04-13 23:35:12 -0400

    Finished the generate_secret_ascii

--- src/lib.rs
+++ src/lib.rs
@@ -13,17 +13,12 @@ pub struct SecretKey {
 pub fn digest() {}
 
 pub fn generate_secret(
-    length: Option<u8>, // Revisit the size of the integer. This is 0-255. We could make it larger.
+    length: Option<u32>,
     symbols: Option<bool>,
     otpauth_url: Option<bool>,
     name: Option<&str>,
     issuer: Option<&str>,
 ) -> SecretKey {
-    println!("{:?}", length);
-    println!("{:?}", symbols);
-    println!("{:?}", otpauth_url);
-    println!("{:?}", name);
-    println!("{:?}", issuer);
     SecretKey { ascii: None, hex: None, base32: None, otpauth_url: None } // TODO (kevinburchfield) - fix. Only did this so it would compile
 }
 
@@ -31,21 +26,39 @@ pub fn get_otp_auth_url(){}
 
 // Helpers
 fn generate_secret_ascii(
-    length: Option<u8>, // Revisit the size of the integer. This is 0-255. We could make it larger.
+    length: Option<u32>,
     symbols: bool
 ) -> String {
+    let byte_array_length = match length {
+        Some(l) => l,
+        None => 32 
+    };
 
-    // NodeJS generates random byte arrays using unsigned 32bit ints - so use u32 here
-    // https://github.com/nodejs/node/blob/e46c680bf2b211bbd52cf959ca17ee98c7f657f5/src/crypto/crypto_random.cc
+    let byte_array: Vec<u8> = (0..byte_array_length).map(|_| { rand::random::<u8>() }).collect();
 
-    static char_set: [char; 62] = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
+    // Static for efficiency rather than leaving it to runtime to init a vector of chars
+    static CHAR_SET: [char; 62] = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
         'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
         'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
     ];
-    static symbol_set: [char; 22] = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '<', '>', '?', '/', '[', ']', '{', '}', ',', '.', ':', ';'];
+    static SYMBOL_SET: [char; 22] = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '<', '>', '?', '/', '[', ']', '{', '}', ',', '.', ':', ';'];
 
-    // TODO (kevinburchfield) - fix. Only did this so it would compile
-    String::from("doing this to compile")
+    let mut secret: String = String::from("");
+    for (_, value) in byte_array.iter().enumerate() {
+        // Need to decide to grab from the symbol/char set if configuration wants to add symbols to secret
+        if symbols {
+            secret.push(
+                match value % 2 {
+                    0 => CHAR_SET[((usize::from(value / 1)) * (CHAR_SET.len() - 1)) / 255],
+                    1 => SYMBOL_SET[(((usize::from(value / 1)) * (SYMBOL_SET.len() - 1))) / 255],
+                    _ => unreachable!("Error: Reached the unreachable match arm of `u8` modulo 2")
+                }
+            )
+        } else {
+            secret.push(CHAR_SET[(((usize::from(value / 1)) * (CHAR_SET.len() - 1))) / 255])
+        }
+    }
+    secret
 }
 
 fn encodeURIComponent() {
@@ -54,8 +67,32 @@ fn encodeURIComponent() {
 
 #[cfg(test)]
 mod tests {
+    use crate::generate_secret_ascii;
+
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn test_generate_secret_ascii_no_symbols() {
+        let secret = generate_secret_ascii(Some(2000), false);
+        assert_eq!(secret.len(), 2000);
+    }
+
+    #[test]
+    fn test_generate_secret_ascii_symbols() {
+        let secret = generate_secret_ascii(Some(2000), true);
+        assert_eq!(secret.len(), 2000);
+
+        // Chances are that a secret of length 2000 will have one arbitrary symbol
+        // TODO (kevinburchfield) - Fix this to check against all of the symbols to be certain
+        assert_eq!(secret.contains("!"), true);
+    }
+
+    #[test]
+    fn test_generate_secret_ascii_no_defined_length() {
+        let secret = generate_secret_ascii(None, false);
+        assert_eq!(secret.len(), 32);
     }
 }
