48518559fac8838a11bcd5d9bcfc57d940367431
Jarrett Helton <jarretthelton@Jarretts-MacBook-Pro.local>
2021-04-14 22:20:40 -0400

    introducing error handling, cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -1,44 +1,74 @@
-use hmac::{
-    crypto_mac::InvalidKeyLength,
-    Hmac, Mac, NewMac,
-};
+use hmac::{crypto_mac, Hmac, Mac, NewMac};
 use sha1::Sha1;
+use sha2::{Sha256, Sha512};
+use thiserror::Error;
 
 type HmacSha1 = Hmac<Sha1>;
+type HmacSha256 = Hmac<Sha256>;
+type HmacSha512 = Hmac<Sha512>;
 
 pub mod hotp;
 pub mod totp;
 
-enum HmacFunction<A> {
+/// GenerationError enumerates all possible errors returned by this library.
+#[derive(Error, Debug)]
+pub enum GenerationError {
+    #[error("Invalid Key Length")]
+    InvalidKeyLength(#[from] crypto_mac::InvalidKeyLength),
+}
+
+enum HmacFunction<A, B, C> {
     Sha1(A),
+    Sha256(B),
+    Sha512(C),
 }
 
 pub enum Algorithm {
     Sha1,
-    // Todo (Jayhelton)Implement other hash functions
-    // Sha256,
-    // Sha512,
+    Sha256,
+    Sha512,
 }
 
+/// Applys a specified keyed hashing function (hmac).
+///
+/// # Arguments
+///
+/// * `secret` - A string of the secret
+/// * `counter` - The counter to hash
+/// * `algorithm` - The preferred algorithm
+///
+/// # Examples
+///
+/// ```
+/// use lugnut::{ digest, Algorithm };
+/// let hash = digest("My secret".to_string(), 5000, Algorithm::Sha1);
+/// ```
 pub fn digest(
     secret: String,
     counter: u128,
     algorithm: Algorithm,
-) -> std::result::Result<Vec<u8>, InvalidKeyLength> {
-    let mac = match get_hmac(secret, algorithm) {
-        Ok(_hmac) => _hmac,
-        Err(e) => return Err(e),
-    };
+) -> std::result::Result<Vec<u8>, GenerationError> {
+    let mac = get_hmac(secret, algorithm)?;
 
+    // Convert the counter into a u8 array of base16 values
     let mut buf = vec![0; 8];
     let mut tmp = counter;
-    for i in 0 ..8 {
-      buf[7 - i] = (tmp & 0xff) as u8;
-      tmp = tmp >> 8;
+    for i in 0..8 {
+        buf[7 - i] = (tmp & 0xff) as u8;
+        tmp = tmp >> 8;
     }
 
+    // Unwrap enum and apply the hmac alg
     match mac {
         HmacFunction::Sha1(mut _mac) => {
+            _mac.update(&buf);
+            Ok(_mac.finalize().into_bytes().to_vec())
+        }
+        HmacFunction::Sha256(mut _mac) => {
+            _mac.update(&buf);
+            Ok(_mac.finalize().into_bytes().to_vec())
+        }
+        HmacFunction::Sha512(mut _mac) => {
             _mac.update(&buf);
             Ok(_mac.finalize().into_bytes().to_vec())
         }
@@ -48,33 +78,26 @@ pub fn digest(
 pub fn generate_secret() {}
 pub fn get_otp_auth_url() {}
 
+#[doc(hidden)]
 fn get_hmac(
     secret: String,
     algorithm: Algorithm,
-) -> std::result::Result<HmacFunction<HmacSha1>, InvalidKeyLength> {
-    let hash = match algorithm {
-        Algorithm::Sha1 => {
-            let hmac = match HmacSha1::new_varkey(secret.as_bytes()) {
-                Ok(_hmac) => _hmac,
-                Err(e) => return Err(e),
-            };
-            HmacFunction::Sha1(hmac)
-        }
-    };
-    Ok(hash)
+) -> std::result::Result<HmacFunction<HmacSha1, HmacSha256, HmacSha512>, GenerationError> {
+    Ok(match algorithm {
+        Algorithm::Sha1 => HmacFunction::Sha1(HmacSha1::new_varkey(secret.as_bytes())?),
+        Algorithm::Sha256 => HmacFunction::Sha256(HmacSha256::new_varkey(secret.as_bytes())?),
+        Algorithm::Sha512 => HmacFunction::Sha512(HmacSha512::new_varkey(secret.as_bytes())?),
+    })
 }
 
 #[cfg(test)]
 mod digest_tests {
     use crate::digest;
+    use crate::Algorithm::Sha1;
 
     #[test]
     fn it_works() {
-        let test = digest(
-            "My secret".to_string(),
-            5000,
-            crate::Algorithm::Sha1,
-        );
+        let test = digest("My secret".to_string(), 5000, Sha1);
         match test {
             Ok(result) => println!("Testing {:02x?}", result),
             Err(_) => panic!("There was an error in the test"),
