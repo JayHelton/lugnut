b5e7a318cba7ddf7cc46c7e0362af0f08cb4eb02
Jarrett Helton <jarretthelton@Jarretts-MacBook-Pro.local>
2021-04-26 21:15:38 -0400

    initil alg

--- src/totp/mod.rs
+++ src/totp/mod.rs
@@ -1,3 +1,12 @@
+use std::time::{SystemTime, UNIX_EPOCH};
+
 pub fn generate() {}
 pub fn verify() {}
 pub fn verify_delta() {}
+fn get_counter() -> u64 {
+  let start = SystemTime::now();
+  let since_the_epoch = start
+      .duration_since(UNIX_EPOCH)
+      .unwrap(); // TODO for simplicity, im gong to unwrap here instead of catch errors. If this throws an error, it might indicate worse issues
+  since_the_epoch.as_secs() / 30
+}
