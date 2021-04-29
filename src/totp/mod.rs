12ce4a3e1563cef279107119f81a527c3cce7092
jayhelton <jaydhelton@gmail.com>
2021-04-28 20:29:26 -0400

    totp initially works

--- src/totp/mod.rs
+++ src/totp/mod.rs
@@ -4,19 +4,14 @@ use crate::{generate, verify_delta, GenerationError};
 
 pub struct Totp {
     key: String,
-    counter: u64,
     time_offset: Option<u64>,
     digest: Option<Vec<u8>>,
 }
 
 impl Totp {
     pub fn new(key: String) -> Totp {
-        let start = SystemTime::now();
-        let since_the_epoch = start.duration_since(UNIX_EPOCH).unwrap();
-        let counter = since_the_epoch.as_secs() / 30;
         Totp {
             key,
-            counter,
             time_offset: None,
             digest: None,
         }
@@ -33,22 +28,41 @@ impl Totp {
     }
 
     pub fn generate<'a>(&'a self) -> std::result::Result<String, GenerationError> {
-        generate(
+        generate(self.key.clone(), get_counter() as u128, None, self.digest.clone())
+    }
+
+    pub fn verify<'a>(&'a mut self, token: String) -> std::result::Result<bool, GenerationError> {
+        let counter = get_counter();
+        verify_delta(
+            token,
             self.key.clone(),
-            self.counter as u128,
+            counter as u128 ,
             None,
+            Some(0), // TODO make window a parameter
             self.digest.clone(),
         )
     }
+}
 
-    // pub fn verify<'a>(&'a mut self, token: String) -> std::result::Result<bool, GenerationError> {
-    //     // verify_delta(
-    //     //     token,
-    //     //     self.key.clone(),
-    //     //     self.counter,
-    //     //     None,
-    //     //     None,
-    //     //     self.digest.clone(),
-    //     // )
-    // }
+fn get_counter() -> u64 {
+    let start = SystemTime::now();
+    let epoch = start.duration_since(UNIX_EPOCH).unwrap();
+    epoch.as_secs() / 30
+}
+
+#[cfg(test)]
+mod totp_tests {
+    use std::{thread, time::Duration};
+
+    use super::Totp;
+
+    #[test]
+    fn it_works() {
+        let mut totp = Totp::new("my secret key".to_string());
+        let code = totp.generate().expect("borked");
+        println!(" {:?}", code);
+        thread::sleep(Duration::from_secs(31));
+        let verified = totp.verify(code).expect("borked here too");
+        println!(" {:?}", verified);
+    }
 }
